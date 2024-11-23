9d7d37d0c81f89fba706eb4cad1071875271b0d0
jlpinoc <114681555+jlpinoc@users.noreply.github.com>
2024-11-22 21:49:59 -0500

    Update LecturaBinarioArchivo.cpp
    Se agrego la funcion mostrar y correccion en las tildes debido a error en pantalla

--- 13_archivos/LecturaBinarioArchivo.cpp
+++ 13_archivos/LecturaBinarioArchivo.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include "registro.h"
+
 using namespace std;
+
+// Declaración de la función
+void mostrar(const tRegistro& registro);
+
 int main() {
-	tRegistro registro;
-	fstream archivo;
-	archivo.open("bd.dat", ios::in | ios::binary);
-	archivo.read( (char *) &registro, SIZE);
-	int cuantos = archivo.gcount();
-	while (cuantos == SIZE) {
-		mostrar(registro); //Función mostrar registro
-		archivo.read( (char *) &registro, SIZE);
-		cuantos = archivo.gcount();
-	}
-	archivo.close();
-	return 0;
+    tRegistro registro;
+    fstream archivo;
+
+    archivo.open("bd.dat", ios::in | ios::binary);
+    if (!archivo.is_open()) {
+        cerr << "Error al abrir el archivo." << endl;
+        return 1;
+    }
+
+    archivo.read((char*)&registro, SIZE);
+    int cuantos = archivo.gcount();
+    while (cuantos == SIZE) {
+        mostrar(registro); // Llamada a la función mostrar
+        archivo.read((char*)&registro, SIZE);
+        cuantos = archivo.gcount();
+    }
+
+    archivo.close();
+    return 0;
 }
 
-const int MAX= 80;
-struct tRegistro{
-	int codigo;
-	char itema[MAX];
-	double valor;
-};
-int SIZE = sizeof(tRegistro);
+// Implementación de la función mostrar
+void mostrar(const tRegistro& registro) {
+    cout << "Codigo: " << registro.codigo << endl;
+    cout << "Nombre: " << registro.itema << endl;
+    cout << "Precio: " << registro.valor << endl;
+    cout << "-------------------" << endl;
+}
